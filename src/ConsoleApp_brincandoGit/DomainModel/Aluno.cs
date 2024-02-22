84d9160b9270dead553b02d78636158430f139fb
edmilson.hora <edmilson.hora@gpnet.com.br>
2024-02-22 10:46:20 -0300

    MinhasAlteracoes

---
+++ src/ConsoleApp_brincandoGit/DomainModel/Aluno.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp_brincandoGit.DomainModel
+{
+    public class Aluno : EntityBase
+    {
+        public string Nome { get; set; }
+        public string RA { get; set; }
+    }
+}
